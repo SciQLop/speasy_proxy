229aa8d810f666d28fd044b5a555ebb4cd1d3077
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-01-08 09:36:39 +0100

    Added detailed logs for each request
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- spwc_proxy/views/get_data.py
+++ spwc_proxy/views/get_data.py
@@ -2,6 +2,11 @@ from pyramid.view import view_config
 from pyramid.response import Response
 import pickle
 import spwc
+from datetime import datetime
+from spwc import SpwcVariable
+import logging
+
+log = logging.getLogger(__name__)
 
 
 @view_config(route_name='get_data', renderer='json')
@@ -10,11 +15,24 @@ def get_data(request):
     for parameter in ("path", "start_time", "stop_time"):
         value = request.params.get(parameter, None)
         if value is None:
+            log.error('Missing parameter: {name}'.format(name=parameter))
             return Response(
                 content_type="text/plain",
                 body="Error: missing {name} parameter".format(name=parameter)
             )
         params[parameter] = value
-    var = spwc.get_data(**params)
+    log.debug('New request: {path} {start_time} {stop_time}'.format(**params))
+    var: SpwcVariable = spwc.get_data(**params)
+    if var is not None:
+        if len(var.time):
+            log.debug(
+                'Got data: data shape = {shape}, data start time = {start_time}, data stop time = {stop_time}'.format(
+                    shape=var.data.shape, start_time=datetime.fromtimestamp(var.time[0]),
+                    stop_time=datetime.fromtimestamp(var.time[-1])))
+        else:
+            log.debug('Got empty data')
+    else:
+        log.debug('Got None')
     result = pickle.dumps(var)
+    del var
     return Response(content_type="text/plain", body=result)
