ca80398200a3d04334fcf684850e78582d5e87b2
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2020-01-11 18:33:32 +0100

    Removed Pyramid default stuff from welcome page and added few stats
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ requires = [
     'pyramid_jinja2',
     'pyramid_debugtoolbar',
     'waitress',
+    'humanize',
     'spwc',
 ]
 
--- spwc_proxy/views/default.py
+++ spwc_proxy/views/default.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_config
+from spwc import cache
+from humanize import filesize
 
 
 @view_config(route_name='home', renderer='../templates/welcome.jinja2')
 def my_view(request):
-    return {'project': 'spwc-proxy'}
+    return {'entries' : cache.cache_len(), 'cache_disk_size':filesize.naturalsize(cache.cache_disk_size())}
