300bd99e1ddd092c877610df0521d5f13d323722
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-09-02 21:23:17 +0200

    few fixes for production container
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- docker/entry_point.sh
+++ docker/entry_point.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 
-python3.11 /home/speasy/speasy_proxy/speasy_config.py --log-level $SPEASY_PROXY_LOG_LEVEL --cache-size $SPEASY_CACHE_SIZE --cache-path $SPEASY_CACHE_PATH --index-path $SPEASY_INDEX_PATH --enable-proxy $SPEASY_PROXY --proxy-prefix $SPEASY_PROXY_PREFIX --server-port $PORT --proxy-url $SPEASY_PROXY_URL --config-path /home/speasy/speasy_proxy/docker.ini
-python3.11 ~/.local/bin/pserve /home/speasy/speasy_proxy/docker.ini
+python3 /home/speasy/speasy_proxy/speasy_config.py --log-level $SPEASY_PROXY_LOG_LEVEL --cache-size $SPEASY_CACHE_SIZE --cache-path $SPEASY_CACHE_PATH --index-path $SPEASY_INDEX_PATH --enable-proxy $SPEASY_PROXY --proxy-prefix $SPEASY_PROXY_PREFIX --server-port $PORT --proxy-url $SPEASY_PROXY_URL --config-path /home/speasy/speasy_proxy/docker.ini
+python3 ~/.local/bin/pserve /home/speasy/speasy_proxy/docker.ini
