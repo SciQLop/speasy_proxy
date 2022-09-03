3087b01ff1c27285c540d0cedf97719d9c701840
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-09-03 18:21:48 +0200

    Small fixes around bokeh backend
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy_proxy/bokeh_backend.py
+++ speasy_proxy/bokeh_backend.py
@@ -2,13 +2,13 @@ from math import prod
 
 from jinja2 import Template
 from speasy.products.variable import SpeasyVariable
-from speasy import SpeasyIndex
+from speasy import SpeasyIndex, provider_and_product
 from bokeh.plotting import figure
 from bokeh.models import CrosshairTool, DataRange1d, HoverTool, ColumnDataSource, CustomJS, Div
-from bokeh.layouts import column, row
+from bokeh.layouts import column
 from bokeh.events import RangesUpdate
-from bokeh.resources import CDN, INLINE
-from bokeh.embed import file_html, components
+from bokeh.resources import INLINE
+from bokeh.embed import components
 from bokeh.palettes import Set1_9 as palette
 import itertools
 import logging
@@ -78,10 +78,8 @@ if ((last_range[0] > xr.start) || (last_range[1] < xr.end))
 """)
 
 
-def _plot_vector(plot, product, data, host_url, request_url):
+def _plot_vector(plot, provider_uid, product_uid, data, host_url, request_url):
     if len(data) > 0:
-        if isinstance(product, SpeasyIndex):
-            product = f"{product.spz_provider()}/{product.spz_uid()}"
         colors = itertools.cycle(palette)
         if len(data.columns) != data.values.shape[1]:
             columns = [f'component {i}' for i in range(data.values.shape[1])]
@@ -103,7 +101,8 @@ def _plot_vector(plot, product, data, host_url, request_url):
             )
         js = JS_TEMPLATE.render(columns=columns, start=str(data.time[0]), stop=str(data.time[-1]))
         callback = CustomJS(
-            args=dict(source=source, xr=plot.x_range, product=product, plot_title=plot.title, request_url=request_url,
+            args=dict(source=source, xr=plot.x_range, product=f"{provider_uid}/{product_uid}", plot_title=plot.title,
+                      request_url=request_url,
                       server_url=host_url),
             code=js)
         plot.js_on_event(RangesUpdate, callback)
@@ -111,7 +110,7 @@ def _plot_vector(plot, product, data, host_url, request_url):
         plot.legend.click_policy = "hide"
 
 
-def _plot_spectrogram(plot, product, data: SpeasyVariable, host_url, request_url):
+def _plot_spectrogram(plot, provider_uid, product_uid, data: SpeasyVariable, host_url, request_url):
     import matplotlib.pyplot as plt
     import matplotlib.colors as colors
     if len(data) > 0:
@@ -120,7 +119,8 @@ def _plot_spectrogram(plot, product, data: SpeasyVariable, host_url, request_url
 
         cm = plt.pcolormesh(data.axes[0], data.axes[1].T, data.values.T,
                             cmap='plasma',
-                            norm=colors.LogNorm(vmin=np.nanmin(data.values[np.nonzero(data.values)]), vmax=np.nanmax(data.values)))
+                            norm=colors.LogNorm(vmin=np.nanmin(data.values[np.nonzero(data.values)]),
+                                                vmax=np.nanmax(data.values)))
         flat_cmap = cm.cmap(cm.norm(cm.get_array()))
         image = np.empty((data.values.shape[1], data.values.shape[0]), dtype=np.uint32)
         view = image.view(dtype=np.uint8).reshape((image.shape[0], image.shape[1], 4))
@@ -136,39 +136,42 @@ def _plot_spectrogram(plot, product, data: SpeasyVariable, host_url, request_url
 
 
 def plot_data(product, data: SpeasyVariable, request):
-    if data is not None and len(data) > 0:
-        data.replace_fillval_by_nan(inplace=True)
-        y_axis_type = SCALES_LUT.get(data.meta.get('SCALETYP', 'linear').lower(), 'linear')
-        plot = figure(plot_width=900, plot_height=500, x_axis_type="datetime", sizing_mode='stretch_both',
-                      y_axis_type=y_axis_type
-                      )
-        if isinstance(product, SpeasyIndex):
-            title_text_template = f"{product.spz_uid()} from {product.spz_provider()}"
-        else:
-            title_text_template = f"{product}"
+    provider_uid, product_uid = provider_and_product(product)
+    try:
+        if data is not None and len(data) > 0:
+            data.replace_fillval_by_nan(inplace=True)
+            y_axis_type = SCALES_LUT.get(data.meta.get('SCALETYP', 'linear').lower(), 'linear')
+            plot = figure(plot_width=900, plot_height=500, x_axis_type="datetime", sizing_mode='stretch_both',
+                          y_axis_type=y_axis_type
+                          )
 
-        plot.title.text = title_text_template
+            plot.title.text = f"{product_uid} from {provider_uid}"
 
-        plot.title.align = "center"
-        plot.title.text_font_size = "25px"
-        plot.xaxis.axis_label = 'Time'
+            plot.title.align = "center"
+            plot.title.text_font_size = "25px"
+            plot.xaxis.axis_label = 'Time'
 
-        plot.add_tools(CrosshairTool())
+            plot.add_tools(CrosshairTool())
 
-        request_url = Div(
-            text=f'<a href="{request.application_url}/get_data?format=html_bokeh&path={product}&start_time={str(data.time[0])}&stop_time={str(data.time[-1])}">Plot URL</a>')
+            request_url = Div(
+                text=f'<a href="{request.application_url}/get_data?format=html_bokeh&path={provider_uid}/{product_uid}&start_time={str(data.time[0])}&stop_time={str(data.time[-1])}">Plot URL</a>')
 
-        if len(data.values.shape) == 2 and (
-                data.meta.get('DISPLAY_TYPE', '') == 'spectrogram' or data.values.shape[1] > 10):
-            _plot_spectrogram(plot, product, data, host_url=request.application_url, request_url=request_url)
-        elif len(data.values.shape) == 2:
-            _plot_vector(plot, product, data, host_url=request.application_url, request_url=request_url)
+            if len(data.values.shape) == 2 and (
+                    data.meta.get('DISPLAY_TYPE', '') == 'spectrogram' or data.values.shape[1] > 10):
+                _plot_spectrogram(plot, provider_uid, product_uid, data, host_url=request.application_url,
+                                  request_url=request_url)
+            elif len(data.values.shape) == 2:
+                _plot_vector(plot, provider_uid, product_uid, data, host_url=request.application_url,
+                             request_url=request_url)
 
-        script, div = components(column(request_url, plot, sizing_mode='stretch_width'))
-        html = TEMPLATE.render(plot_script=script,
-                               plot_div=div,
-                               js_resources=INLINE.render_js(),
-                               css_resources=INLINE.render_css())
-        return html
+            script, div = components(column(request_url, plot, sizing_mode='stretch_width'))
+            html = TEMPLATE.render(plot_script=script,
+                                   plot_div=div,
+                                   js_resources=INLINE.render_js(),
+                                   css_resources=INLINE.render_css())
+            return html
 
-    log.debug(f"Can't plot {product}, data shape: {data.values.shape if data is not None else None}")
+        log.debug(f"Can't plot {product}, data shape: {data.values.shape if data is not None else None}")
+    except:
+        pass
+    return f"Oops, unable to plot {product_uid} from {provider_uid}"
