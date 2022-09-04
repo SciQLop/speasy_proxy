402abdf082b6cc688a7637ac4ae421ded805b509
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2022-09-04 17:21:02 +0200

    Bump version: 0.4.1 → 0.5.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ tests_require = [
 
 setup(
     name='speasy_proxy',
-    version='0.4.1',
+    version='0.5.0',
     description='speasy-proxy',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
