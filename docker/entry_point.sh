9b38cb791ab15d7ff94c53e566b11e48955b068d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-11-19 15:27:49 +0100

    Docker image listen on all interfaces by default
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- docker/entry_point.sh
+++ docker/entry_point.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-~/.local/bin/uvicorn speasy_proxy:app --root-path $SPEASY_PROXY_PREFIX --port $PORT --proxy-headers --limit-concurrency 10000 --log-config /home/speasy/speasy_proxy/logging.yaml
+~/.local/bin/uvicorn speasy_proxy:app --root-path $SPEASY_PROXY_PREFIX --host "0.0.0.0" --port $PORT --proxy-headers --limit-concurrency 10000 --log-config /home/speasy/speasy_proxy/logging.yaml
