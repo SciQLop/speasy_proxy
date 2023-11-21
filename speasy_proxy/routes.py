ac1820f198fc4a80f30581ea30a4a857dd7583ee
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-11-21 10:20:51 +0100

    cleanup some pyramid leftovers
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy_proxy/routes.py
+++
@@ -1,9 +0,0 @@
-def includeme(config):
-    config.add_static_view('static', 'speasy_proxy:static', cache_max_age=3600)
-    config.add_route('home', '/')
-    config.add_route('get_data', '/get_data')
-    config.add_route('get_cache_entries', '/get_cache_entries')
-    config.add_route('get_speasy_version', '/get_speasy_version')
-    config.add_route('get_version', '/get_version')
-    config.add_route('get_inventory', '/get_inventory')
-    config.add_route('chart_roulette', '/chart_roulette')
