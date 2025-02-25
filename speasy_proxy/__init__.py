53864dd17478933bbc02f24e4615c5676166d7c5
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-02-25 15:56:22 +0100

    Bump version: 0.12.0 → 0.12.1

--- speasy_proxy/__init__.py
+++ speasy_proxy/__init__.py
@@ -1,6 +1,6 @@
 __author__ = """Alexis Jeandet"""
 __email__ = 'alexis.jeandet@member.fsf.org'
-__version__ = '0.12.0'
+__version__ = '0.12.1'
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
