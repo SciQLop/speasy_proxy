5ef8e3c1db576c958dc63da6f8c319fba40819e4
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-03-14 15:00:29 +0100

    Add config switch to enable/disable collab endpoint
    By default, speasy_proxy users only want the cache feature, let's
    make collab endpoint an opt-in feature.
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy_proxy/config/__init__.py
+++ speasy_proxy/config/__init__.py
@@ -1,3 +1,7 @@
 from speasy.config import ConfigSection
 
 index = ConfigSection("PROXY_INDEX", path={"default": "/tmp"})
+collab_endpoint = ConfigSection("PROXY_ENABLE_COLLAB_ENDPOINT",
+                                enable={"default": False,
+                                        "type_ctor": lambda x: {'true': True,
+                                                                'false': False}.get(x.lower(), False)})
