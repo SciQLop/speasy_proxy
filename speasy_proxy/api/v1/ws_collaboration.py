3c2c7e7e112a76cda792840127c4f76894186fe3
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-03-14 15:00:29 +0100

    Add config switch to enable/disable collab endpoint
    By default, speasy_proxy users only want the cache feature, let's
    make collab endpoint an opt-in feature.
    
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- speasy_proxy/api/v1/ws_collaboration.py
+++ speasy_proxy/api/v1/ws_collaboration.py
@@ -1,26 +1,33 @@
-from asyncio import create_task
+from speasy_proxy.config import collab_endpoint
+import logging
 
-from fastapi import WebSocket
-from pycrdt_websocket import WebsocketServer
-from pycrdt_websocket.websocket import HttpxWebsocket
+log = logging.getLogger(__name__)
 
-from .routes import router
-
-
-@router.websocket("/collaboration")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    websocket_server = await get_websocket_server()
-    await websocket_server.serve(HttpxWebsocket(websocket, "collaboration"))
+if collab_endpoint.enable():
+    from asyncio import create_task
+    from fastapi import WebSocket
+    from pycrdt_websocket import WebsocketServer
+    from pycrdt_websocket.websocket import HttpxWebsocket
+    from .routes import router
 
 
-async def get_websocket_server():
-    global WEBSOCKET_SERVER
-    if WEBSOCKET_SERVER is None:
-        WEBSOCKET_SERVER = WebsocketServer()
-        create_task(WEBSOCKET_SERVER.start())
-        await WEBSOCKET_SERVER.started.wait()
-    return  WEBSOCKET_SERVER
+    @router.websocket("/collaboration")
+    async def websocket_endpoint(websocket: WebSocket):
+        await websocket.accept()
+        websocket_server = await get_websocket_server()
+        await websocket_server.serve(HttpxWebsocket(websocket, "collaboration"))
 
 
-WEBSOCKET_SERVER = None
+    async def get_websocket_server():
+        global WEBSOCKET_SERVER
+        if WEBSOCKET_SERVER is None:
+            WEBSOCKET_SERVER = WebsocketServer()
+            create_task(WEBSOCKET_SERVER.start())
+            await WEBSOCKET_SERVER.started.wait()
+        return WEBSOCKET_SERVER
+
+
+    WEBSOCKET_SERVER = None
+
+else:
+    log.info(f'Collaboration endpoint is disabled, set {collab_endpoint.enable.env_var_name} to True to enable it')
